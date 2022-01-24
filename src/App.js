317a12c5cb4b705bb71555f811e4c9f7693d6776
mehrab-haque <mehrab.haque.0001@gmail.com>
2022-01-24 16:20:14 +0600

    boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Audio } from  'react-loader-spinner'
+import React, {useState} from "react";
+import {Dialog, DialogContent} from "@mui/material";
+import Landing from "./components/Landing";
+
+var showToast;
+var setLoading;
 
 function App() {
+
+  const [loading, setL] = useState(false);
+  setLoading = setL;
+  showToast = (message) => {
+    toast.dark(message, {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <ToastContainer
+            position="bottom-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+        />
+        <Dialog open={loading}>
+          <DialogContent>
+              <Audio color="#00BFFF" height={100} width={100} />
+          </DialogContent>
+        </Dialog>
+        <Landing/>
+
+      </div>
+  )
 }
 
 export default App;
+export { showToast, setLoading};
---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react'
+
+const Landing=props=>{
+
+    return(
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default Landing
